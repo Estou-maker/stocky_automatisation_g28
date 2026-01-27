f1dbadd49d60629495be60ea240944149ac0cf94
Estou-maker <eslemtouhemi@gmail.com>
2026-01-27 21:53:58 +0100

    new push

---
+++ cypress/e2e/Page_Objects/login_Objects.js
@@ -0,0 +1,11 @@
+const loginElements = {
+    loginURL: 'https://mystocky.uksouth.cloudapp.azure.com/auth',
+    TitleCNX: 'div[data-testid="login-welcome-text"]',
+    EmailInput: 'input[data-testid="input-identifier"]',
+    PasswordInput: 'p-password[data-testid="input-password"]',
+    submitBtn: 'p-button[data-testid="btn-submit"]',
+    DashboardContainer: 'employee-default[class="ng-star-inserted"]',
+    ErrorMessage: 'div[data-testid="login-card"]'
+}
+
+export default loginElements;
